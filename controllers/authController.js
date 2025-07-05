180d54029d6dad448d8ca00ef67715ad5e1e0d01
iamtheharsh <harshdhansinghpatel@gmail.com>
2025-07-05 16:19:10 +0530

    set up loginuser

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,17 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { generateToken } = require('../utils/generateToken');
 
+// Register User
 module.exports.registerUser = async function (req, res) {
   try {
     const { email, password, fullname } = req.body;
 
     bcrypt.genSalt(10, async function (err, salt) {
       if (err) return res.status(500).send(err.message);
-      let user = await userModel.findOne({email : email});
-      if(user){
-        return res.status(401).send("You already have account ,please log in ")
+
+      let existingUser = await userModel.findOne({ email });
+      if (existingUser) {
+        return res.status(401).send("You already have an account, please log in");
       }
 
       bcrypt.hash(password, salt, async function (err, hash) {
@@ -26,14 +28,36 @@ module.exports.registerUser = async function (req, res) {
 
           let token = generateToken(user);
           res.cookie("token", token);
-
-          res.send("user created successfully");
+          return res.send("User created successfully");
         } catch (err) {
-          res.status(500).send(err.message);
+          return res.status(500).send(err.message);
         }
       });
     });
   } catch (err) {
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message);
+  }
+};
+
+// Login User
+module.exports.loginUser = async function (req, res) {
+  try {
+    const { email, password } = req.body;
+
+    let user = await userModel.findOne({ email : email});
+    if (!user) {
+      return res.status(401).send("Email or password incorrect");
+    }
+
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (err) return res.status(500).send(err.message);
+      if (!result) return res.status(401).send("Email or password incorrect");
+
+      let token = generateToken(user);
+      res.cookie("token", token);
+      return res.send("Logged in successfully");
+    });
+  } catch (err) {
+    return res.status(500).send(err.message);
   }
 };
